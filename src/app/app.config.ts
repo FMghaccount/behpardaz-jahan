6b935e1a96dab5875a4fa353403ea520f9df858d
Farzin mossaed <farzinmossaed@gmail.com>
2024-11-29 17:27:15 +0330

    final Project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CurrencyPipe, registerLocaleData } from '@angular/common';
+import localeFa from '@angular/common/locales/fa';
+
+registerLocaleData(localeFa);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    CurrencyPipe,
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'fa-IR' },
+  ],
 };
